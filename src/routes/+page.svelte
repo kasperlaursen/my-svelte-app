58e8600c83c3e3ebbb11a3cefb7e7d1a382d019a
kasperlaursen <habberlabber@gmail.com>
2023-03-16 13:16:54 +0100

    Get data and show prices nicely ish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import PriceList from '../lib/PriceList.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Welcome to my app</h1>
+
+<PriceList prices={data.prices} />
+
+<code>
+	<pre>{JSON.stringify(data.prices, null, 2)}</pre>
+</code>
